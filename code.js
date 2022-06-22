53d978ef808c42c6872b47cee6a3c2bcf1201401
Azubuike Uche <102628983+kellz59@users.noreply.github.com>
2022-06-22 19:01:28 +0600

    Add files via upload

---
+++ code.js
@@ -0,0 +1,3 @@
+document.getElementById("name").innerHTML = "Name:Azubuike Uche"; 
+document.getElementById("height").innerHTML = "Height: 6.0cm";
+document.getElementById("country").innerHTML = "Country: Nigeria";
